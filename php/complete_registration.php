d769f1db89a91ced45267db3d88da7defbfa5aae
Artho-dev <loic.ganne@etu.univ-amu.fr>
2021-01-16 22:30:02 +0100

    finition de la completion d'inscription par email

--- php/complete_registration.php
+++ php/complete_registration.php
@@ -1,8 +1,6 @@
 <?php
 
-include_once 'models/Model.php';
-
-echo '<h1>Inscription Terminée</h1>';
+include_once '../models/Model.php';
 
 if(isset($_GET['id'], $_GET['code']) && !empty($_GET['id']) && !empty($_GET['code'])) {
     $id = (int) $_GET['id'];
@@ -17,6 +15,7 @@ if(isset($_GET['id'], $_GET['code']) && !empty($_GET['id']) && !empty($_GET['cod
         deleteRegisterConfirmationRequest($id, $code);
 
         //TODO On affiche la View
+        echo '<h1>Inscription Terminée</h1>';
     }
     else {
         die('Erreur Fatale : Requête introuvable');
