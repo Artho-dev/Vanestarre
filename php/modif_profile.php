d58d58c8a9a8637a72332be521e92e23e0851168
Rastidev <62051512+Rastidev@users.noreply.github.com>
2021-01-28 20:29:20 +0100

    new file

---
+++ php/modif_profile.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once '../models/Model.php';
+
+
+if(){
+	$username = (string) $_POST['username'];
+	$name = (string) $_POST['name'];
+	$mail = (string) $_POST['mail'];
+	$birthday = $_POST['birthday'];
+	$bio = (string) $_POST['bio'];
+	
+	$user = getUserById($userid);
+	$profile = getProfile($userid);
+	
+	if($username == $user['username']) {
+		if(!updateUsername($userid, $username)){
+			//error
+		}
+	}
+	
+	if($name == $user['name']) { updatePseudo($userid, $name); }
+	
+	if($mail == $user['mail']) {
+		if(!updateMail($userid, $mail)){
+			//error
+		}
+	}
+	
+	if($birthday == $user['birthday']) { updateBirthday($userid, $birthday); }
+	
+	if($bio == $profile['description']) { updateBio($userid, $bio); }
+	
+	
+}
+
