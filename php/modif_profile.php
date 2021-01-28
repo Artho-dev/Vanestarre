bf5c5d7ef15df8f80819b5cd8559df3b79957195
Rastidev <62051512+Rastidev@users.noreply.github.com>
2021-01-28 22:15:35 +0100

    Ca marche !!!!!

--- php/modif_profile.php
+++ php/modif_profile.php
@@ -1,35 +1,39 @@
 <?php
 
 include_once '../models/Model.php';
+
+require_once '../header.php';
 require_once 'connexion_handler.php';
 
 if(true){
+	$userid = $sessionid;
+	
 	$username = (string) $_POST['username'];
 	$name = (string) $_POST['name'];
 	$mail = (string) $_POST['mail'];
 	$birthday = $_POST['birthdate'];
 	$bio = (string) $_POST['description'];
 	
-	$user = getUserById($sessionid);
-	$profile = getProfile($sessionid);
+	$user = getUserById($userid);
+	$profile = getProfile($userid);
 	
-	if($username == $user['username']) {
-		if(!updateUsername($sessionid, $username)){
+	if($username != $user['username']) {
+		if(!updateUsername($userid, $username)){
 			//error
 		}
 	}
 	
-	if($name == $user['name']) { updatePseudo($sessionid, $name); }
+	if($name != $user['name']) { updatePseudo($userid, $name); }
 	
-	if($mail == $user['mail']) {
-		if(!updateMail($sessionid, $mail)){
+	if($mail != $user['mail']) {
+		if(!updateMail($userid, $mail)){
 			//error
 		}
 	}
 	
-	if($birthday == $user['birthday']) { updateBirthday($sessionid, $birthday); }
+	if($birthday != $user['birthday']) { updateBirthday($userid, $birthday); }
 
-	if($bio == $profile['description']) { updateBio($sessionid, $bio); }
+	if($bio != $profile['description']) { updateBio($userid, $bio); }
 
 	
 }
